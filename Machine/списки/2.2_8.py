58627fe290c3a313360fc1bb811987d2c332e609
Filippenko Dmitry <dmitfilippenko.work@gmail.com>
2024-10-31 00:19:49 +0400

    ML 2.2_8, 2.2_9

--- Machine/списки/2.2_8.py
+++ Machine/списки/2.2_8.py
@@ -1,19 +1,8 @@
-def reverse_segment(n, arr):
-    # индекс левого минимума
-    min_value = min(arr)
-    left_min_index = arr.index(min_value)
+def main():
+    n = int(input())
+    a = list(map(int, input().split()))
+    g = (len(a) - a[::-1].index(max(a))-1)
+    print(*(a[:min(a.index(min(a)), (len(a) - a[::-1].index(max(a)) - 1))] + a[min(a.index(min(a)), (len(a) - a[::-1].index(max(a))-1)):max(a.index(min(a)), (len(a) - a[::-1].index(max(a))-1))+1][::-1] + a[max(a.index(min(a)), (len(a) - a[::-1].index(max(a))-1))+1:]))
 
-    # индекс правого максимума
-    max_value = max(arr)
-    right_max_index = len(arr) - 1 - arr[::-1].index(max_value)
 
-    # инвертируем порядок элементов на участке между left_min_index и right_max_index
-    if left_min_index <= right_max_index:
-        arr[left_min_index:right_max_index + 1] = arr[left_min_index:right_max_index + 1][::-1]
-
-    return arr
-
-n = int(input())
-arr = list(map(int, input().split()))
-result = reverse_segment(n, arr)
-print(" ".join(map(str, result)))
+main()
